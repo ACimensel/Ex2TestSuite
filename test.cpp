3a46eb747e7de490968a8876697a6308782d6fb5
Alvin <ho.alvin857@gmail.com>
2022-10-11 18:50:36 -0700

    Finished up test.cpp
    fixed file path and fixed comparison

--- test.cpp
+++ test.cpp
@@ -11,23 +11,35 @@ bool matchFiles(const string& outputFile);
 
 int main(){
     // TODO print whether test failed or passed
-
-    test("input1.csv", "output1.xml");
-    test("input2.csv", "output2.xml");
+    bool test1_result = test("input1.csv", "output1.xml");
+    bool test2_result = test("input2.csv", "output2.xml");
+    bool test3_result = test("input3.csv", "output3.xml");
+    // bool test_result = test("input1.csv", "output1.xml");
+    // bool test1_result = test("input1.csv", "output1.xml");
+    cout << "\n";
+    printf("Test 1: %s \n", test1_result ? "Pass" : "Fail");
+    printf("Test 2: %s \n", test2_result ? "Pass" : "Fail");
+    printf("Test 3: %s \n", test3_result ? "Pass" : "Fail");
+    // printf("Test 1: %s \n", test("input1.csv", "output1.xml") ? "Pass" : "Fail");
+    // printf("Test 1: %s \n", test("input1.csv", "output1.xml") ? "Pass" : "Fail");
+    // printf("Test 1: %s \n", test("input1.csv", "output1.xml") ? "Pass" : "Fail");
+    //test("input2.csv", "output2.xml");
     //test("input3.csv", "output3.xml");
     //test("input4.csv", "output4.xml");
     //test("input5.csv", "output5.xml");
     //test("input6.csv", "output6.xml");
+    cout << "Enter any string to escape.\n";
+    getchar();
 }
 
 bool test(const string& inputFile, const string& outputFile){
     string jarFilePath = "";
     string javaJar = "java -jar ";
-    string inputLoc = ".\\TestInputs\\" + inputFile + ' ';
-    string outputLoc = ".\\TestOutputs\\" + outputFile + ' ';
+    string inputLoc = "TestInputs\\" + inputFile + ' ';
+    string outputLoc = "TestOutputs\\" + outputFile + ' ';
     string jarFile = "csvxmlconverter.jar ";
     string xslFile = "transform.xsl ";
-    string stylizedOutputFile = ".\\TestOutputs\\s_" + outputFile;
+    string stylizedOutputFile = "TestOutputs\\s_" + outputFile;
 
     jarFilePath.assign(javaJar);
     jarFilePath.append(jarFile + inputLoc + outputLoc + jarFile + xslFile + stylizedOutputFile + " > file.txt");
@@ -35,7 +47,7 @@ bool test(const string& inputFile, const string& outputFile){
     printf("Jar execution path: %s\n\n", jarFilePath.c_str());
     system(jarFilePath.c_str());
 
-    return matchFiles(outputFile); // TODO should return fail if the system call didn't create the output file
+    return matchFiles(outputFile); 
 }
 
 bool matchFiles(const string& outputFile) {
@@ -44,22 +56,26 @@ bool matchFiles(const string& outputFile) {
     ofstream file3;
     string line1, line2;
 
-    file1.open(".\\TestOutputs\\exp_" + outputFile);
-    file2.open(".\\ExpectedOutputs\\exp_" + outputFile);
+    file1.open("TestOutputs\\" + outputFile);
+    file2.open("ExpectedOutputs\\exp_" + outputFile);
     if (file1.fail()) { cerr << "Error opening file !!" << endl; return false; }
     if (file2.fail()) { cerr << "Error opening file !!" << endl; return false; }
 
     while (!file1.eof()){
         getline(file1, line1);
         getline(file2, line2);
-        if (line1 != line2) {
+        if (line1.compare(line2) != 0) {
+            cout << "Not the same line: \n";
+            cout << line1 << "\n";
+            cout << line2 << "\n\n";
             return false;
         }
     }
 
-    /*if (!file2.eof()) {
-        printf("File 2 not finished");
-    }*/
+    if (!file2.eof()) {
+        printf("Not thee same length\n");
+        return false;
+    }
 
     file1.close();
     file2.close();
